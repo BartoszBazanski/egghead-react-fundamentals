352e4963afaed5a0c8e4c32699d688091d089623
Bartosz Bazański <b.bazanski@visuality.pl>
2017-05-11 11:30:24 +0200

    Referencing different components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {a: ''};
+  }
+  update() {
+    this.setState({
+      a: this.a.refs.input.value,
+      b: this.b.refs.input.value
+    });
+  }
   render() {
-    return <Title text="React"/>
+    return (
+      <div>
+        <Input
+          ref={ component => this.a = component }
+          update={this.update.bind(this)}
+        /> {this.state.a}
+        <hr/>
+        <Input
+          ref={ component => this.b = component }
+          update={this.update.bind(this)}
+        /> {this.state.b}
+      </div>
+    )
   }
 }
-
-const Title = (props) => <h1>Title: {props.text}</h1>;
-
-Title.propTypes = {
-  text(props, propName, component) {
-    if(!(propName in props)) {
-      return new Error(`missing ${propName}`);
-    }
-    if(props[propName].length < 6) {
-      return new Error(`${propName} was too short`);
-    }
+class Input extends React.Component {
+  render() {
+    return <div><input ref="input" type="text" onChange={this.props.update} /></div>
   }
-};
+}
 
 export default App;
