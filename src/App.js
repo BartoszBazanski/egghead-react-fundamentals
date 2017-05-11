11e1b841400650c9d1bd33b305ffd97af3ef1d67
Bartosz Bazański <b.bazanski@visuality.pl>
2017-05-11 17:32:39 +0200

    Use React.cloneElements to extend functionality of children components

--- src/App.js
+++ src/App.js
@@ -1,40 +1,37 @@
 import React from 'react';
-import './App.css';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      input: '/* add you jsx here */',
-      output: '',
-      err: ''
-    };
-  }
-  update(e) {
-    let code = e.target.value;
-    try {
-      this.setState({
-        output: window.Babel.transform(code, { presets: ['es2015', 'react'] }).code,
-        err: ''
-      });
-    } catch (err) {
-      this.setState({err: err.message});
-    }
-  }
   render() {
     return (
-      <div>
-        <header>{this.state.err}</header>
-        <div className="container">
-          <textarea
-          onChange={this.update.bind(this)}
-          defaultValue={this.state.input}/>
-          <pre>
-            {this.state.output}
-          </pre>
-        </div>
-      </div>
+      <Buttons>
+        <button value="A">A</button>
+        <button value="B">B</button>
+        <button value="C">C</button>
+      </Buttons>
     );
+  }
+}
+
+class Buttons extends React.Component {
+  constructor() {
+    super();
+    this.state = {selected: 'None'};
+  }
+  selectItem(selected) {
+    this.setState({selected});
+  }
+  render() {
+    let fn = child =>
+      React.cloneElement(child, {
+        onClick: this.selectItem.bind(this, child.props.value)
+      });
+    let items = React.Children.map(this.props.children, fn);
+    return (
+      <div>
+        <h2>You have Selected: {this.state.selected}</h2>
+        {items}
+      </div>
+    )
   }
 }
 
